ff9bdf22dd3fbf175b2e217d112fdd6704ed236a
Luke Kuzmish <lukekuzmish@gmail.com>
2011-10-19 22:29:45 -0400

    converted to OOP, no new functionality

--- string_of_4.oop.php
+++ string_of_4.oop.php
@@ -11,7 +11,7 @@ class String_Of_4 {
     var $side_length;
 
 
-    private function generate_random_string($size = 12) {
+   public function generate_random_string($size = 12) {
       $available_chars = "0123456789abcdefghijklmnopqrstuvwxyz";
       $to_return = "";
       
@@ -25,9 +25,36 @@ class String_Of_4 {
     }
 
 
-    public function output_json() {
+    public function return_json() {
       // TODO
+      header('Content-type: text/json');
+      header('Content-type: application/json');
+/*
+$side_length = 8;
+$game_state = array(
+    "player1_color"   =>  "",
+    "player2_color"   =>  "",
+    "gameboard"       =>  array(),
+    "next_move"       =>  "",
+    "status"          =>  "new",
+    "game_id"         =>  ""
+);
+*/
+
+      $to_return = array(
+        "player1_color" => $this->player1_color,
+        "player2_color" => $this->player2_color, 
+        "gameboard"     => $this->gameboard,
+        "next_move"     => $this->next_move,
+        "status"        => $this->status,
+        "game_id"       => $this->game_id        
+      );
+      print json_encode($to_return);
+      die;
     }
+
+
+
 
 
     function __construct($player1_color_choice, $_side_length = 8) {
@@ -52,12 +79,13 @@ class String_Of_4 {
 
 
       // setup gameboard multidimensional array
-      for ($i = 0; $i < $side_length; $i++) {
-        for ($j = 0; $j < $side_length; $j++) {
+      for ($i = 0; $i < $this->side_length; $i++) {
+        for ($j = 0; $j < $this->side_length; $j++) {
           $row[$j] = 0;
         }
         $this->gameboard[$i] = $row;
       }
+
 
 
       // decide which player goes first
@@ -69,21 +97,12 @@ class String_Of_4 {
 
       
       // create string id
-      $this->game_id = generate_random_string();
+      $this->game_id = $this->generate_random_string(12);
       
     } // __construct
 
+}
 
-/*
-$side_length = 8;
-$game_state = array(
-    "player1_color"   =>  "",
-    "player2_color"   =>  "",
-    "gameboard"       =>  array(),
-    "next_move"       =>  "",
-    "status"          =>  "new",
-    "game_id"         =>  ""
-);
-*/
-
+$my_game = new String_Of_4("red");
+$my_game->return_json();
 ?>

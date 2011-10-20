66881612c1ecd9bd88ae663791656ff5f4033731
Luke Kuzmish <lukekuzmish@gmail.com>
2011-10-20 01:17:06 -0400

    added make_move method, needs functionality for adding to filled column

--- string_of_4.oop.php
+++ string_of_4.oop.php
@@ -4,6 +4,19 @@ require_once("config.php");
 
 class String_Of_4 {
 
+/*
+
+  Board setup
+      0   1   2   3   4   5   6   7       gameboard[column][row]
+ 7   [ ] [ ] [ ] [ ] [ ] [ ] [ ] [B]      A  =  gameboard[0][0]
+ 6   [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]      B  =  gameboard[7][7]
+ 5   [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]      C  =  gameboard[3][1]
+ 4   [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+ 3   [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+ 2   [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+ 1   [ ] [ ] [ ] [C] [ ] [ ] [ ] [ ]
+ 0   [A] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+*/
     private $player1_color;
     private $player2_color;
     private $gameboard = array();
@@ -18,42 +31,37 @@ class String_Of_4 {
         $this->load_from_file($filename);
       }
 
-      else {
-        $row = array();
 
-        
+      else {
         // set colors
         $this->player1_color = $player1_color_choice;
         
         switch ($player1_color_choice) {
-          case "red":
-            $this->player2_color = "black";
+          case "r":
+            $this->player2_color = "b";
             break;
-          case "black":
-            $this->player2_color = "red";
+          case "b":
+            $this->player2_color = "r";
             break;
           default:
             // error
         }
 
-
+        $col = array();
         // setup gameboard multidimensional array
         for ($i = 0; $i < SIDE_LENGTH; $i++) {
           for ($j = 0; $j < SIDE_LENGTH; $j++) {
-            $row[$j] = 0;
+            $col[$j] = 0;
           }
-          $this->gameboard[$i] = $row;
+          $this->gameboard[$i] = $col;
         }
-
 
 
         // decide which player goes first
         $this->next_move = mt_rand(1,2);
 
-
         // set status
         $this->status = "awaiting";
-
         
         // create string id
         $this->game_id = $this->generate_random_string(12);
@@ -106,7 +114,7 @@ class String_Of_4 {
       $contents = fread($fp, filesize($file_loc));
       fclose($fp);
 
-      $arr_vars = json_decode($contents,true);
+      $arr_vars = json_decode($contents,true);    // true makes array associative
       $this->player1_color = $arr_vars["player1_color"];
       $this->player2_color = $arr_vars["player2_color"];
       $this->gameboard = $arr_vars["gameboard"];
@@ -115,23 +123,44 @@ class String_Of_4 {
       $this->game_id = $arr_vars["game_id"];        
 
     }
-    /*
-$filename = "Somefile.txt"; //creating our file name
-$filehandle = fopen($filename, "w"); //create a file and a file handle (description)
-fwrite($filehandle, "Dummy data"); //writing into the created file. one more parameter available
-fclose($filehandle); //finish the first file handle, no need to close it. just for demonstration
 
-$fileread = fopen($filename, "r"); //create a second handle (which is the same like the first)
-$filetext = fread($filename, filesize($filename)); //read *all* data from the file
-fclose($flieread); //end handle
-echo $filetext; //echo the file data
 
-      */
+    public function make_move($col) {
+
+      // check for bad input
+      if (($col >= SIDE_LENGTH) || ($col < 0)) {
+        return false;
+      }
 
       
-  
+      if ($this->next_move == 1) {
+        $drop_color = $this->player1_color;
+      }
+      else {
+        $drop_color = $this->player2_color;
+      }
+
+      foreach ($this->gameboard[$col] as &$slot) {
+        if ($slot == 0) {
+          $slot = $drop_color;
+          return true;
+        }
+      // TODO
+      // tried adding to a column that is full
+      // RETURN_AN_ERROR_WITH_JSON
+      // disable this in the UI too
+      }
+
+
+    }
+    
 }
 
-$my_game = new String_Of_4("red","trial");
+
+
+$my_game = new String_Of_4("r","trial");
+if ($my_game->make_move(1) == true) {
+  // TODO successfully dropped, now switch players
+}
 $my_game->return_json();
 ?>
